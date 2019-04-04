cedd66af19ab8f6bd0ca65d91a35e187beb6873d
buddycoding <dummyemail12313123@gmail.com>
2019-04-04 14:13:03 +0800

    1st commit

--- app/src/main/java/com/example/myapplication/ChordProgressionActivity.java
+++ app/src/main/java/com/example/myapplication/ChordProgressionActivity.java
@@ -10,4 +10,6 @@ public class ChordProgressionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chord_progression);
     }
+
+    //test
 }
