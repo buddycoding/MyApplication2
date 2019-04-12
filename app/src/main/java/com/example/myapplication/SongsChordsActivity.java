3506df166ed92f23c7cef90e9820da5f45e172f3
buddycoding <dummyemail12313123@gmail.com>
2019-04-12 19:39:02 +0800

    update onbackpressed, zoom images

--- app/src/main/java/com/example/myapplication/SongsChordsActivity.java
+++ app/src/main/java/com/example/myapplication/SongsChordsActivity.java
@@ -3,12 +3,16 @@ package com.example.myapplication;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.github.chrisbanes.photoview.PhotoView;
 
 
 public class SongsChordsActivity extends AppCompatActivity {
+
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +26,16 @@ public class SongsChordsActivity extends AppCompatActivity {
         songName.setText(myIntent.getStringExtra("SongName"));
         artistName.setText(myIntent.getStringExtra("ArtistName"));
 
-        PhotoView photoView = (PhotoView) findViewById(R.id.pv_songimage);
+        PhotoView photoView = findViewById(R.id.pv_songimage);
         photoView.setImageResource(R.drawable.canthelpfallinginlove);
+
+        button1 = findViewById(R.id.buttonhome);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
 
     }
 }
